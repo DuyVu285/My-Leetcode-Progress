50ec0ba6f7bbe56fd36ab793af76543909bad4d2
DuyVu285 <80942615+DuyVu285@users.noreply.github.com>
2023-06-19 01:36:02 +0700

    Time: 15 ms (75.50%), Space: 13.4 MB (63.90%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        lowercase_word = s.lower()
+        return lowercase_word
