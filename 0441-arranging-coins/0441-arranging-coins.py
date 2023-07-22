2d2cd0aeb471a2595b1e765815e1b9ecbf03e8fb
Vũ Nhật Duy <80942615+DuyVu285@users.noreply.github.com>
2023-07-22 12:33:52 +0700

    Time: 29 ms (65.22%), Space: 13.3 MB (58.58%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int((-1 + math.sqrt(1 + 8 * n)) // 2) 
+                
+                
+            
+        
