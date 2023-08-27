52aa44d6adc140e9773931e2e2c3e9888252c30e
Vũ Nhật Duy <80942615+DuyVu285@users.noreply.github.com>
2023-08-27 14:15:22 +0700

    Time: 15 ms (85.01%), Space: 13.2 MB (89.91%) - LeetHub

---
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        d = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+             "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+        return len({''.join(d[ord(i) - ord('a')] for i in w) for w in words})
