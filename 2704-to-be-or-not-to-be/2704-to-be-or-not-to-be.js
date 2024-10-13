a194d54fb526e04389dc8a100bde1bb0fecf4b87
Vũ Nhật Duy <80942615+DuyVu285@users.noreply.github.com>
2024-10-13 16:53:35 +0700

    Time: 51 ms (63.22%), Space: 48.4 MB (90.60%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -3,14 +3,23 @@
  * @return {Object}
  */
 var expect = function(val) {
+
     return {
         toBe: (val2) => {
-            if(val !== val2) throw new Error("Not Equal");
-            else return true;
+            if (val !== val2) {
+                throw new Error("Not Equal");
+            }
+            else {
+                return true
+            }
         },
         notToBe: (val2) => {
-            if(val === val2) throw new Error("Equal");
-            else return true;
+            if (val === val2){
+                throw new Error("Equal");
+            }
+            else{
+                return true
+            }
         }
     }
 };
