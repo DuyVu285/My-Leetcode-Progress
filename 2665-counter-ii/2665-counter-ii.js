75deb05ec955d6af7a6f3e3735fa9bb5fd6984d5
Vũ Nhật Duy <80942615+DuyVu285@users.noreply.github.com>
2024-10-13 17:04:52 +0700

    Time: 47 ms (96.90%), Space: 51.8 MB (48.10%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -3,21 +3,21 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
-    let presentCount = init;
-
-  function increment() {
-    return ++presentCount;
-  }
-
-  function decrement() {
-      return --presentCount;
-  }
-
-  function reset() {
-      return (presentCount = init);
-  }
-
-  return { increment, decrement, reset };
+    let numVal = init;
+    return {
+        increment: () => {
+            numVal = numVal + 1;
+            return numVal;
+        },
+        reset: () => {
+            numVal = init;
+            return numVal;
+        },
+        decrement: () => {
+            numVal = numVal - 1;
+            return numVal;
+        }
+    }
 };
 
 /**
