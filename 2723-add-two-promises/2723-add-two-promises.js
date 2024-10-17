611f5c9455bd25022bdf381abbba8d161d8f07b0
Vũ Nhật Duy <80942615+DuyVu285@users.noreply.github.com>
2024-10-17 13:44:35 +0700

    Time: 66 ms (39.26%), Space: 49.6 MB (42.58%) - LeetHub

---
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    let result = 0;
+    result = await promise1 + await promise2;
+    return result;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
