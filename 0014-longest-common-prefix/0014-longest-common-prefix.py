a677a5a25c084eaa1480e2d932ae870e122a8adf
Vũ Nhật Duy <80942615+DuyVu285@users.noreply.github.com>
2024-11-05 13:16:36 +0700

    Time: 0 ms (100.00%), Space: 16.7 MB (11.99%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,21 +1,12 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
-
-        prefix = ""
-        min_length = min(len(word) for word in strs)
-
-        for i in range(min_length):
-            first_char = strs[0][i]
-            if all(word[i] == first_char for word in strs):
-                prefix += first_char
-            else:
-                break
-
+        
+        prefix = strs[0]
+        for string in strs[1:]:
+            while not string.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
         return prefix
-                
