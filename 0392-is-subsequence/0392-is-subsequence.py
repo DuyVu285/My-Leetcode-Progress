1520f1d34a21de21f640fa622c75b9bb0b958255
Vũ Nhật Duy <80942615+DuyVu285@users.noreply.github.com>
2024-11-06 14:36:09 +0700

    Time: 3 ms (27.32%), Space: 16.5 MB (76.73%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,16 +1,9 @@
-class Solution(object):
-    def isSubsequence(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        s_pointer = 0
-        t_pointer = 0
-
-        while s_pointer < len(s) and t_pointer < len(t):
-            if s[s_pointer] == t[t_pointer]:
-                s_pointer += 1
-            t_pointer += 1
-
-        return s_pointer == len(s)
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0,0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j+= 1
+        
+        return i == len(s)
