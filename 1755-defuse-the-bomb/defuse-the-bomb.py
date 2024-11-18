3ca6abe7c70cf50a2138793b5b675f4709edeb8f
Vũ Nhật Duy <80942615+DuyVu285@users.noreply.github.com>
2024-11-18 18:12:46 +0700

    Time: 0 ms (100.00%) | Memory: 16.9 MB (5.59%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.py
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -2,20 +2,22 @@ class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
         n = len(code)
         decrypted = [0] * n
+
+        if k == 0:
+            return decrypted
+
         if k > 0:
+            current_sum = sum(code[1 : k + 1])
             for i in range(n):
-                j = i+1
-                element_sum = 0
-                for j in range(1,k+1):
-                    element_sum += code[(i + j) % n]
-                decrypted[i] = element_sum
-        elif k < 0:
-            for i in range(len(code)):
-                j = i-1
-                element_sum = 0
-                for j in range(1, abs(k) + 1):
-                    element_sum += code[(i-j)%n]
-                decrypted[i] = element_sum
-        else:
-            decrypted = [0] * n
+                decrypted[i] = current_sum
+                current_sum -= code[(i + 1) % n]
+                current_sum += code[(i + k + 1) % n]
+        if k < 0:
+            k = -k
+            current_sum = sum(code[-k:])
+            for i in range(n):
+                decrypted[i] = current_sum
+                current_sum -= code[(i - k) % n]
+                current_sum += code[i % n]
+
         return decrypted
