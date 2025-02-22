ae5d4d4ebd0b63599ec5eacb973c8b84d9612a9f
Vũ Nhật Duy <80942615+DuyVu285@users.noreply.github.com>
2025-02-22 14:52:34 +0700

    Time: 19 ms (64.41%) | Memory: 18.3 MB (80.29%) - LeetSync

---
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        stack = []
+        i = 0
+
+        while i < len(traversal):
+            depth = 0
+
+            while i < len(traversal) and traversal[i] == "-":
+                depth += 1
+                i += 1
+
+            value_start = i
+            while i < len(traversal) and traversal[i].isdigit():
+                i += 1
+            value = int(traversal[value_start:i])
+
+            node = TreeNode(value)
+
+            if depth == len(stack):
+                if stack:
+                    stack[-1].left = node
+            else:
+                stack = stack[:depth]
+                stack[-1].right = node
+            
+            stack.append(node)
+
+        return stack[0]
+
+        
